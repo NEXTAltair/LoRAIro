33d7631965715d7bf01c7557927156564facbd7c
NEXTAltair <users.noreply.github.com>
2025-12-22 11:48:11 +0000

    taskはserena､Cipherに映したので削除

--- tools/check_annotate_model.py
+++
@@ -1,8 +0,0 @@
-"""image_annotator_lib 現在使えるモデルめいを表示"""
-
-from image_annotator_lib import list_available_annotators
-
-annotators = list_available_annotators()
-
-for annotator in annotators:
-    print(annotator)
