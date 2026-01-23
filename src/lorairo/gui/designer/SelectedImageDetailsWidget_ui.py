84f799380263dc0b62a16942efbe7ebd2f3c0b59
NEXTAltair <users.noreply.github.com>
2026-01-23 09:25:17 +0000

    Refactor documentation and UI code
    - Updated reStructuredText files for consistency in formatting and structure across various modules.
    - Improved the readability of the documentation by ensuring uniformity in headings and indentation.
    - Enhanced the SelectedImageDetailsWidget UI code by applying consistent naming conventions and object properties.
    - Removed deprecated comments and ensured that the code adheres to PEP 8 style guidelines.
    - Added UTF-8 encoding declaration to the SelectedImageDetailsWidget UI file for better compatibility.

--- src/lorairo/gui/designer/SelectedImageDetailsWidget_ui.py
+++ src/lorairo/gui/designer/SelectedImageDetailsWidget_ui.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ################################################################################
 ## Form generated from reading UI file 'SelectedImageDetailsWidget.ui'
 ##
@@ -6,75 +8,41 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    Qt,
-    QTime,
-    QUrl,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QGridLayout,
-    QGroupBox,
-    QLabel,
-    QSizePolicy,
-    QTabWidget,
-    QTextEdit,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLabel,
+    QSizePolicy, QTabWidget, QTextEdit, QVBoxLayout,
+    QWidget)
 
 from ..widgets.annotation_data_display_widget import AnnotationDataDisplayWidget
 
-
-class Ui_SelectedImageDetailsWidget:
+class Ui_SelectedImageDetailsWidget(object):
     def setupUi(self, SelectedImageDetailsWidget):
         if not SelectedImageDetailsWidget.objectName():
-            SelectedImageDetailsWidget.setObjectName("SelectedImageDetailsWidget")
+            SelectedImageDetailsWidget.setObjectName(u"SelectedImageDetailsWidget")
         SelectedImageDetailsWidget.resize(250, 400)
         self.verticalLayoutMain = QVBoxLayout(SelectedImageDetailsWidget)
         self.verticalLayoutMain.setSpacing(6)
-        self.verticalLayoutMain.setObjectName("verticalLayoutMain")
+        self.verticalLayoutMain.setObjectName(u"verticalLayoutMain")
         self.verticalLayoutMain.setContentsMargins(6, 6, 6, 6)
         self.tabWidgetDetails = QTabWidget(SelectedImageDetailsWidget)
-        self.tabWidgetDetails.setObjectName("tabWidgetDetails")
+        self.tabWidgetDetails.setObjectName(u"tabWidgetDetails")
         self.tabOverview = QWidget()
-        self.tabOverview.setObjectName("tabOverview")
+        self.tabOverview.setObjectName(u"tabOverview")
         self.verticalLayoutOverview = QVBoxLayout(self.tabOverview)
-        self.verticalLayoutOverview.setObjectName("verticalLayoutOverview")
+        self.verticalLayoutOverview.setObjectName(u"verticalLayoutOverview")
         self.groupBoxImageInfo = QGroupBox(self.tabOverview)
-        self.groupBoxImageInfo.setObjectName("groupBoxImageInfo")
+        self.groupBoxImageInfo.setObjectName(u"groupBoxImageInfo")
         self.gridLayoutImageInfo = QGridLayout(self.groupBoxImageInfo)
         self.gridLayoutImageInfo.setSpacing(3)
-        self.gridLayoutImageInfo.setObjectName("gridLayoutImageInfo")
+        self.gridLayoutImageInfo.setObjectName(u"gridLayoutImageInfo")
         self.labelFileName = QLabel(self.groupBoxImageInfo)
-        self.labelFileName.setObjectName("labelFileName")
+        self.labelFileName.setObjectName(u"labelFileName")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -84,14 +52,14 @@ class Ui_SelectedImageDetailsWidget:
         self.gridLayoutImageInfo.addWidget(self.labelFileName, 0, 0, 1, 1)
 
         self.labelFileSize = QLabel(self.groupBoxImageInfo)
-        self.labelFileSize.setObjectName("labelFileSize")
+        self.labelFileSize.setObjectName(u"labelFileSize")
         sizePolicy.setHeightForWidth(self.labelFileSize.sizePolicy().hasHeightForWidth())
         self.labelFileSize.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelFileSize, 2, 0, 1, 1)
 
         self.labelFileNameValue = QLabel(self.groupBoxImageInfo)
-        self.labelFileNameValue.setObjectName("labelFileNameValue")
+        self.labelFileNameValue.setObjectName(u"labelFileNameValue")
         self.labelFileNameValue.setWordWrap(True)
         sizePolicy.setHeightForWidth(self.labelFileNameValue.sizePolicy().hasHeightForWidth())
         self.labelFileNameValue.setSizePolicy(sizePolicy)
@@ -99,72 +67,74 @@ class Ui_SelectedImageDetailsWidget:
         self.gridLayoutImageInfo.addWidget(self.labelFileNameValue, 0, 1, 1, 1)
 
         self.labelFileSizeValue = QLabel(self.groupBoxImageInfo)
-        self.labelFileSizeValue.setObjectName("labelFileSizeValue")
+        self.labelFileSizeValue.setObjectName(u"labelFileSizeValue")
         sizePolicy.setHeightForWidth(self.labelFileSizeValue.sizePolicy().hasHeightForWidth())
         self.labelFileSizeValue.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelFileSizeValue, 2, 1, 1, 1)
 
         self.labelCreatedDate = QLabel(self.groupBoxImageInfo)
-        self.labelCreatedDate.setObjectName("labelCreatedDate")
+        self.labelCreatedDate.setObjectName(u"labelCreatedDate")
         sizePolicy.setHeightForWidth(self.labelCreatedDate.sizePolicy().hasHeightForWidth())
         self.labelCreatedDate.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelCreatedDate, 3, 0, 1, 1)
 
         self.labelCreatedDateValue = QLabel(self.groupBoxImageInfo)
-        self.labelCreatedDateValue.setObjectName("labelCreatedDateValue")
+        self.labelCreatedDateValue.setObjectName(u"labelCreatedDateValue")
         sizePolicy.setHeightForWidth(self.labelCreatedDateValue.sizePolicy().hasHeightForWidth())
         self.labelCreatedDateValue.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelCreatedDateValue, 3, 1, 1, 1)
 
         self.labelImageSizeValue = QLabel(self.groupBoxImageInfo)
-        self.labelImageSizeValue.setObjectName("labelImageSizeValue")
+        self.labelImageSizeValue.setObjectName(u"labelImageSizeValue")
         sizePolicy.setHeightForWidth(self.labelImageSizeValue.sizePolicy().hasHeightForWidth())
         self.labelImageSizeValue.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelImageSizeValue, 1, 1, 1, 1)
 
         self.labelImageSize = QLabel(self.groupBoxImageInfo)
-        self.labelImageSize.setObjectName("labelImageSize")
+        self.labelImageSize.setObjectName(u"labelImageSize")
         sizePolicy.setHeightForWidth(self.labelImageSize.sizePolicy().hasHeightForWidth())
         self.labelImageSize.setSizePolicy(sizePolicy)
 
         self.gridLayoutImageInfo.addWidget(self.labelImageSize, 1, 0, 1, 1)
 
+
         self.verticalLayoutOverview.addWidget(self.groupBoxImageInfo)
 
         self.tabWidgetDetails.addTab(self.tabOverview, "")
         self.tabTags = QWidget()
-        self.tabTags.setObjectName("tabTags")
+        self.tabTags.setObjectName(u"tabTags")
         self.verticalLayoutTagsTab = QVBoxLayout(self.tabTags)
-        self.verticalLayoutTagsTab.setObjectName("verticalLayoutTagsTab")
+        self.verticalLayoutTagsTab.setObjectName(u"verticalLayoutTagsTab")
         self.groupBoxTags = QGroupBox(self.tabTags)
-        self.groupBoxTags.setObjectName("groupBoxTags")
+        self.groupBoxTags.setObjectName(u"groupBoxTags")
         self.verticalLayoutTags = QVBoxLayout(self.groupBoxTags)
-        self.verticalLayoutTags.setObjectName("verticalLayoutTags")
+        self.verticalLayoutTags.setObjectName(u"verticalLayoutTags")
         self.labelTagsContent = QLabel(self.groupBoxTags)
-        self.labelTagsContent.setObjectName("labelTagsContent")
+        self.labelTagsContent.setObjectName(u"labelTagsContent")
         self.labelTagsContent.setWordWrap(True)
         sizePolicy.setHeightForWidth(self.labelTagsContent.sizePolicy().hasHeightForWidth())
         self.labelTagsContent.setSizePolicy(sizePolicy)
 
         self.verticalLayoutTags.addWidget(self.labelTagsContent)
 
+
         self.verticalLayoutTagsTab.addWidget(self.groupBoxTags)
 
         self.tabWidgetDetails.addTab(self.tabTags, "")
         self.tabCaptions = QWidget()
-        self.tabCaptions.setObjectName("tabCaptions")
+        self.tabCaptions.setObjectName(u"tabCaptions")
         self.verticalLayoutCaptionsTab = QVBoxLayout(self.tabCaptions)
-        self.verticalLayoutCaptionsTab.setObjectName("verticalLayoutCaptionsTab")
+        self.verticalLayoutCaptionsTab.setObjectName(u"verticalLayoutCaptionsTab")
         self.groupBoxCaptions = QGroupBox(self.tabCaptions)
-        self.groupBoxCaptions.setObjectName("groupBoxCaptions")
+        self.groupBoxCaptions.setObjectName(u"groupBoxCaptions")
         self.verticalLayoutCaptions = QVBoxLayout(self.groupBoxCaptions)
-        self.verticalLayoutCaptions.setObjectName("verticalLayoutCaptions")
+        self.verticalLayoutCaptions.setObjectName(u"verticalLayoutCaptions")
         self.textEditCaptionsContent = QTextEdit(self.groupBoxCaptions)
-        self.textEditCaptionsContent.setObjectName("textEditCaptionsContent")
+        self.textEditCaptionsContent.setObjectName(u"textEditCaptionsContent")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(1)
@@ -174,15 +144,16 @@ class Ui_SelectedImageDetailsWidget:
 
         self.verticalLayoutCaptions.addWidget(self.textEditCaptionsContent)
 
+
         self.verticalLayoutCaptionsTab.addWidget(self.groupBoxCaptions)
 
         self.tabWidgetDetails.addTab(self.tabCaptions, "")
         self.tabMetadata = QWidget()
-        self.tabMetadata.setObjectName("tabMetadata")
+        self.tabMetadata.setObjectName(u"tabMetadata")
         self.verticalLayoutMetadata = QVBoxLayout(self.tabMetadata)
-        self.verticalLayoutMetadata.setObjectName("verticalLayoutMetadata")
+        self.verticalLayoutMetadata.setObjectName(u"verticalLayoutMetadata")
         self.annotationDataDisplay = AnnotationDataDisplayWidget(self.tabMetadata)
-        self.annotationDataDisplay.setObjectName("annotationDataDisplay")
+        self.annotationDataDisplay.setObjectName(u"annotationDataDisplay")
         sizePolicy1.setHeightForWidth(self.annotationDataDisplay.sizePolicy().hasHeightForWidth())
         self.annotationDataDisplay.setSizePolicy(sizePolicy1)
 
@@ -192,82 +163,33 @@ class Ui_SelectedImageDetailsWidget:
 
         self.verticalLayoutMain.addWidget(self.tabWidgetDetails)
 
+
         self.retranslateUi(SelectedImageDetailsWidget)
 
         self.tabWidgetDetails.setCurrentIndex(0)
 
-        QMetaObject.connectSlotsByName(SelectedImageDetailsWidget)
 
+        QMetaObject.connectSlotsByName(SelectedImageDetailsWidget)
     # setupUi
 
     def retranslateUi(self, SelectedImageDetailsWidget):
-        SelectedImageDetailsWidget.setWindowTitle(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "Selected Image Details", None)
-        )
-        self.groupBoxImageInfo.setTitle(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u753b\u50cf\u60c5\u5831", None)
-        )
-        self.labelFileName.setText(
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget", "\u30d5\u30a1\u30a4\u30eb\u540d:", None
-            )
-        )
-        self.labelFileSize.setText(
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget", "\u30d5\u30a1\u30a4\u30eb\u30b5\u30a4\u30ba:", None
-            )
-        )
-        self.labelFileNameValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", "-", None))
-        self.labelFileSizeValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", "-", None))
-        self.labelCreatedDate.setText(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u767b\u9332\u65e5:", None)
-        )
-        self.labelCreatedDateValue.setText(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "-", None)
-        )
-        self.labelImageSizeValue.setText(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "-", None)
-        )
-        self.labelImageSize.setText(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u89e3\u50cf\u5ea6:", None)
-        )
-        self.tabWidgetDetails.setTabText(
-            self.tabWidgetDetails.indexOf(self.tabOverview),
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u6982\u8981", None),
-        )
-        self.groupBoxTags.setTitle(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u30bf\u30b0", None)
-        )
-        self.labelTagsContent.setText(
-            QCoreApplication.translate("SelectedImageDetailsWidget", "cat, sitting, outdoor, wooden", None)
-        )
-        self.tabWidgetDetails.setTabText(
-            self.tabWidgetDetails.indexOf(self.tabTags),
-            QCoreApplication.translate("SelectedImageDetailsWidget", "\u30bf\u30b0", None),
-        )
-        self.groupBoxCaptions.setTitle(
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget", "\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3", None
-            )
-        )
-        self.textEditCaptionsContent.setPlaceholderText(
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget",
-                "\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3\u304c\u8868\u793a\u3055\u308c\u307e\u3059",
-                None,
-            )
-        )
-        self.tabWidgetDetails.setTabText(
-            self.tabWidgetDetails.indexOf(self.tabCaptions),
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget", "\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3", None
-            ),
-        )
-        self.tabWidgetDetails.setTabText(
-            self.tabWidgetDetails.indexOf(self.tabMetadata),
-            QCoreApplication.translate(
-                "SelectedImageDetailsWidget", "\u30e1\u30bf\u30c7\u30fc\u30bf", None
-            ),
-        )
-
+        SelectedImageDetailsWidget.setWindowTitle(QCoreApplication.translate("SelectedImageDetailsWidget", u"Selected Image Details", None))
+        self.groupBoxImageInfo.setTitle(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u753b\u50cf\u60c5\u5831", None))
+        self.labelFileName.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30d5\u30a1\u30a4\u30eb\u540d:", None))
+        self.labelFileSize.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30d5\u30a1\u30a4\u30eb\u30b5\u30a4\u30ba:", None))
+        self.labelFileNameValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"-", None))
+        self.labelFileSizeValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"-", None))
+        self.labelCreatedDate.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u767b\u9332\u65e5:", None))
+        self.labelCreatedDateValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"-", None))
+        self.labelImageSizeValue.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"-", None))
+        self.labelImageSize.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u89e3\u50cf\u5ea6:", None))
+        self.tabWidgetDetails.setTabText(self.tabWidgetDetails.indexOf(self.tabOverview), QCoreApplication.translate("SelectedImageDetailsWidget", u"\u6982\u8981", None))
+        self.groupBoxTags.setTitle(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30bf\u30b0", None))
+        self.labelTagsContent.setText(QCoreApplication.translate("SelectedImageDetailsWidget", u"cat, sitting, outdoor, wooden", None))
+        self.tabWidgetDetails.setTabText(self.tabWidgetDetails.indexOf(self.tabTags), QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30bf\u30b0", None))
+        self.groupBoxCaptions.setTitle(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3", None))
+        self.textEditCaptionsContent.setPlaceholderText(QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3\u304c\u8868\u793a\u3055\u308c\u307e\u3059", None))
+        self.tabWidgetDetails.setTabText(self.tabWidgetDetails.indexOf(self.tabCaptions), QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30ad\u30e3\u30d7\u30b7\u30e7\u30f3", None))
+        self.tabWidgetDetails.setTabText(self.tabWidgetDetails.indexOf(self.tabMetadata), QCoreApplication.translate("SelectedImageDetailsWidget", u"\u30e1\u30bf\u30c7\u30fc\u30bf", None))
     # retranslateUi
+
